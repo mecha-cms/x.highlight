bab9ad5115d478df0f413b6f421ec437d278037f
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-08-28 19:03:38 +0700

    Maintainable Dependency

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ Highlight Extension for [Mecha](https://github.com/mecha-cms/mecha)
 Release Notes
 -------------
 
-### 2.0.1
+### 2.0.2
 
+ - Moved `vendor` folder to the extension root.
  - [@mecha-cms/mecha#96](https://github.com/mecha-cms/mecha/issues/96)
 
 ### 2.0.0
--- highlight/index.php
+++ highlight/index.php
@@ -1,9 +1,6 @@
 <?php
 
-require __DIR__ . DS . 'engine' . DS . 'i' . DS . '@scrivo' . DS . 'highlight.php' . DS . 'Highlight' . DS . 'Autoloader.php';
-require __DIR__ . DS . 'engine' . DS . 'i' . DS . '@scrivo' . DS . 'highlight.php' . DS . 'HighlightUtilities' . DS . 'functions.php';
-
-spl_autoload_register("\\Highlight\\Autoloader::load");
+require __DIR__ . DS . 'vendor' . DS . 'autoload.php';
 
 require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'asset.php';
 require __DIR__ . DS . 'engine' . DS . 'r' . DS . 'hook.php';
